d7892d3591669d0a5374f37c666b0b4ed7f252cd
Chili Johnson <chili.johnson@system1.bio>
2020-04-04 16:20:44 -0700

    Allows a dynamic base URL and home URL to be set through env vars.

---
+++ site/_data/metadata.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "Chili J",
+  url: process.env.SITE_URL,
+  externalHomeUrl: process.env.EXTERNAL_HOME_URL,
+  description: "Pretty much anything",
+  strapline: "Pretty much anything",
+  feed: {
+    subtitle: "Pretty much anything",
+    filename: "feed.xml",
+    path: "/feed/feed.xml",
+    id: process.env.BLOG_URL,
+  },
+  author: {
+    name: "Chili Johnson",
+    email: "ChiliJohnson@chilij.com",
+  },
+};
